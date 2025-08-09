f115b55fabfd60fa98fa2c76acd36385c6c80579
Mohsan Ali Zafar <mohsanalimohsan649@gmail.com>
2025-08-09 11:53:33 +0500

    add ui setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function  App() {
+  return (
+    <div className='h-screen w-screen bg-slate-200 flex items-center justify-center'c>App</div>
+  )
+}
+
+export default  App
